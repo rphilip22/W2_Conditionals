5777165dc55588c5ac20e2174ffb92eb73fc8e4b
rphilip22 <reuben.philip@student.fairfield.edu>
2026-02-04 15:39:33 -0500

    updates

---
+++ example_4.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+
+if name == "Kai" or name == "Ryan":
+    print(name , "is from CT.")
+
+elif name == "Jack":
+    print(name , "is from NY.")
+
+elif name == "George":
+    print(name , "is from NJ.")
+
+else:
+    print("Location unknown for" , name)
