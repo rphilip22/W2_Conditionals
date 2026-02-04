0df78bbb5e1cbde94352fc174be7e5f0fdc384ac
rphilip22 <reuben.philip@student.fairfield.edu>
2026-02-04 15:48:11 -0500

    updates

--- example_4.py
+++ example_4.py
@@ -1,13 +1,11 @@
 name = input("Enter your name: ")
 
-if name == "Kai" or name == "Ryan":
-    print(name , "is from CT.")
-
-elif name == "Jack":
-    print(name , "is from NY.")
-
-elif name == "George":
-    print(name , "is from NJ.")
-
-else:
-    print("Location unknown for" , name)
+match name:
+    case "Kai":
+        print(name , "is from CT.")
+    case "Ryan":
+        print(name , "is from CT.")
+    case "Jack":
+        print(name , "is from NY.")
+    case _:
+        print("Location unknown for", name)
